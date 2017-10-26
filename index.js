77769e572a249fe0d164d2e344697ab322e5c137
Patrick McClernan <patmccler@gmail.com>
2017-10-26 23:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,29 @@ function nestedTarget() {
 
 function deepestChild() {
   var currentNode =  document.querySelector('div#grand-node')
-  
+  findDeepestChild(currentNode, 0, currentNode, 0)
 
 }
- function findDeepestChild(node, depth, deepestNode, deepestdepth)
+ function findDeepestChild(node, depth, deepestNode, deepestDepth)
  {
    depth++
    var child = node.firstChild
    
+   if(depth > deepestDepth)
+   {
+     deepestDepth = depth
+     deepestNode = node
+   }
+
+
    while(child){
 
      findDeepestChild(child, depth, deepestNode, deepestDepth)
-     
+
      child = child.nextSibling()
    }
 
    return deepestNode
 
-   
+
  }
