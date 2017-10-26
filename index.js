bf1ace9d23102f4839e5672fd576283bed54559d
Patrick McClernan <patmccler@gmail.com>
2017-10-26 23:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,23 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  return document.querySelector('div#grand-node div div div div')
+  var currentNode =  document.querySelector('div#grand-node')
+  
+
 }
+ function findDeepestChild(node, depth, deepestNode, deepestdepth)
+ {
+   depth++
+   var child = node.firstChild
+   
+   while(child){
+
+     findDeepestChild(child, depth, deepestNode, deepestDepth)
+     
+     child = child.nextSibling()
+   }
+
+   return deepestNode
+
+   
+ }
