df2afc467a785f784ea5b4a066b74cc2188e12e8
Patrick McClernan <patmccler@gmail.com>
2017-10-27 16:49:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  var currentNode =  document.querySelector('#grand-node')
+  var deepest = findDeepestChild(currentNode, 0, {depthV:0})
+  return deepest
+}
+
+ function findDeepestChild(node, depth, deepestNodeAndDepth)
+ {
+   //console.log(`current node: #{node}`)
+   depth++
+
+   if(depth > deepestNodeAndDepth.depthV)
+   {
+     deepestNodeAndDepth.depthV = depth
+     deepestNodeAndDepth.node = node
+   }
+
+   var childCount = node.childElementCount
+   if(childCount > 0)
+   {
+     var children = node.children
+     for(let i = 0; i < childCount; i++)
+     {
+       findDeepestChild(children[i], depth, deepestNodeAndDepth)
+     }
+   }
+
+   return deepestNodeAndDepth.node
+ }
+
+ function increaseRankBy(n)
+ {
+   var elemsToIncrease = document.querySelectorAll('.ranked-list li')
+   var thisElem
+   for(let i = 0; i < elemsToIncrease.length; i++)
+   {
+     thisElem = elemsToIncrease[i]
+     thisElem.innerHTML = parseInt(thisElem.innerHTML) + n
+
+   }
+
+
+
+ }
